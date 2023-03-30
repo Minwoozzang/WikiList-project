65f871817dbc26a3c48be42c875abc41763dbb62
Minwoozzang <kminu0819@naver.com>
2023-03-30 15:51:52 +0900

    dev branch start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <BrowserRouter>
+      <div>최상단</div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
